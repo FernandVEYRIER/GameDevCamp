db27e579cf3e264526cd7ab573033f6d676c9ef6
Fernand <fernand.veyrier@epitech.eu>
2015-02-04 14:46:52 +0100

    Pause menu ok

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	//can be acessed from everywhere
+	public static bool isPlaying;
+
+	public GameObject canvasPause;
+	public GameObject canvasVictory;
+
+	// Use this for initialization
+	void Start () 
+	{
+		isPlaying = true;
+		canvasPause.SetActive(false);
+		canvasVictory.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//sets pause
+		if (Input.GetButtonDown("Pause"))
+		{
+			canvasPause.SetActive(!canvasPause.activeSelf);
+			isPlaying = !isPlaying;
+			Time.timeScale = (Time.timeScale == 1) ? 0 : 1;
+		}
+	}
+}
