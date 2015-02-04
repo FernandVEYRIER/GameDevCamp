82700dccebffb817495d847e71b6a81d03c6141d
gaspar_q <quentin.gasparotto@epitech.eu>
2015-02-04 20:11:33 +0100

    commentaires

--- Assets/Scripts/Manage_Menu.cs
+++ Assets/Scripts/Manage_Menu.cs
@@ -12,25 +12,49 @@ public class 						Manage_Menu : MonoBehaviour
 
 	void	Start()
 	{
+		/*
+		**	I set my array of GameObject array whith my two arrays in order to use it
+		**		by choosing one of the two with a parameter in function Launch_animation()
+		*/
+
 		array[0] = Menu_button;
 		array[1] = Settings_button;
+
+		/*
+		**	I make button menu come to the scene by the animations
+		*/
+
 		StartCoroutine(Launch_anim(0, "come"));
 	}
 
 	public void		Settings_press()
 	{
+		/*
+		**	When the setting button is pressed I make the menu button leave
+		**		and come the settings button
+		*/
+
 		StartCoroutine(Launch_anim(0, "leave"));
 		StartCoroutine(Launch_anim(1, "come"));
 	}
 
 	public void		Return_press()
 	{
+		/*
+		**	When the setting button is pressed I make the settings button leave
+		**		and come the menu button
+		*/
+
 		StartCoroutine(Launch_anim(0, "come"));
 		StartCoroutine(Launch_anim(1, "leave"));
 	}
 
 	public void		Quit_press()
 	{
+		/*
+		**	When you click on the quit button... Bitch why do you feel the necessity of reading this comment
+		*/
+
 		Application.Quit();
 	}
 
@@ -41,6 +65,7 @@ public class 						Manage_Menu : MonoBehaviour
 
 	public void		Change_volume()
 	{
+
 		//appeler l'audio manager de fernand
 	}
 
