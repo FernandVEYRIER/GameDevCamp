26768701e3e1b7e936a13296dbbc811233a3a9ca
Fernand <fernand.veyrier@epitech.eu>
2015-02-04 19:51:13 +0100

    Constanty fait de LA MAGIE NOIRE
    "le respect s'est enfuit"

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	//this contains the menu clip
+	public AudioClip menuClip;
+
+	//and this all the other clips
+	public AudioClip [] audioClip;
+
+	[RequireComponent(typeof (AudioSource))]
+	void Start () 
+	{
+		audio.loop = 1;
+		if (Application.loadedLevel == 0)
+			audio.Play();
+		else
+			StartCoroutine(playSongs());
+	}
+	
+	IEnumerator playSongs()
+	{
+		foreach(AudioClip clip in audioClip)
+		{
+			audio.clip = clip;
+			audio.Play();
+			yield return new WaitForSeconds(audio.clip.length);
+		}
+	}
+}
