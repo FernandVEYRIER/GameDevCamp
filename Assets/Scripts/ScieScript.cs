2bfa6f359ff9009253451441b8cc8191d2c14143
Fernand <fernand.veyrier@epitech.eu>
2015-02-11 10:44:09 +0100

    Iunderstand

---
+++ Assets/Scripts/ScieScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScieScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Destroy(col.gameObject);
+		GameObject.Find("_GameManager").GetComponent<GameManager>().Death();
+	}
+}
