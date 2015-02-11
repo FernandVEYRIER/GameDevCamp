a2e9d5a48e364ff2ee425afc6587ad0764377aaf
Fernand <fernand.veyrier@epitech.eu>
2015-02-11 12:20:15 +0100

    eltoto

--- Assets/Scripts/AddCoin.cs
+++ Assets/Scripts/AddCoin.cs
@@ -6,6 +6,9 @@ public class AddCoin : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.collider2D.tag == "Player")
+		{
 			GameObject.Find("_GameManager").GetComponent<GameManager>().coins++;
+			Destroy(this.gameObject);
+		}
 	}
 }
