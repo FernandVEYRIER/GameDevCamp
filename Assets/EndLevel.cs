afa099013a2657e3c892f3d8dda1be043e54fd0f
Fernand <fernand.veyrier@epitech.eu>
2015-02-11 12:57:02 +0100

    tamer

---
+++ Assets/EndLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class EndLevel : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.collider2D.tag == "Player")
+			GameObject.Find("_GameManager").GetComponent<GameManager>().Victory();
+	}
+}
