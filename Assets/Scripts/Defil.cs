816601b9756d4a5ccc0e57c0240bef25238cb138
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-02-11 09:20:17 +0100

    Push de l'extrem

---
+++ Assets/Scripts/Defil.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defil : MonoBehaviour {
+
+	public float	speed;
+	// Update is called once per frame
+	void Update ()
+	{
+		gameObject.GetComponent<MeshRenderer>().material.mainTextureOffset = new Vector2(gameObject.GetComponent<MeshRenderer>().material.mainTextureOffset.x + speed * 0.001f,
+		                                                                                 gameObject.GetComponent<MeshRenderer>().material.mainTextureOffset.y );
+	}
+}
---
+++ Assets/Scripts/MoveDust.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDust : MonoBehaviour {
+
+	public float	max_y;
+	public float	min_y;
+	public float	speed;
+	private bool	up = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (transform.position.y >= max_y)
+			up = false;
+		if (transform.position.y <= min_y)
+			up = true;
+		if (up)
+			transform.Translate (Vector3.up * speed *Time.deltaTime);
+		else
+			transform.Translate (-Vector3.up * speed *Time.deltaTime);
+	}
+}
