86edae19700030e94bb88c79e3768c95d96b1db2
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-02-11 18:57:03 +0100

    Level3

---
+++ Assets/fourbe.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fourbe : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter()
+	{
+		rigidbody2D.isKinematic = false;
+	}
+}
