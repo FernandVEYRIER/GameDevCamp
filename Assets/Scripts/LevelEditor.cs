3314daf0251b6795f19577b9aa4008287395d676
Fernand <fernand.veyrier@epitech.eu>
2015-07-06 17:19:46 +0200

    c'est cadeau constanty

--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class LevelEditor : MonoBehaviour {
 
+	public GameObject background;
+	public GameObject ground;
 	public GameObject []  propTools;
 	GameObject currentTool;
 
 	bool canSpawnObject;
+
+	GameObject go;
 
 	void Start () 
 	{
@@ -19,12 +23,22 @@ public class LevelEditor : MonoBehaviour {
 		{
 			if ( Input.GetMouseButtonDown( 0 ) && canSpawnObject )
 			{
-				GameObject go = (GameObject) Instantiate( currentTool, Camera.main.ScreenToWorldPoint( Input.mousePosition ), Quaternion.identity );
+				go = (GameObject) Instantiate( currentTool, Camera.main.ScreenToWorldPoint( Input.mousePosition ), Quaternion.identity );
 
+				// We reset the Z axis of the obect othewise GameObject will be invisible
 				go.transform.position += new Vector3( 0, 0, Mathf.Abs( Camera.main.transform.position.z ) );
 			}
 		}
 	}
+
+	public void OnMouseDrag()
+	{
+		canSpawnObject = false;
+		if ( currentTool != null )
+			return;
+		background.GetComponent<MeshRenderer>().material.mainTextureOffset -= new Vector2( Input.GetAxis( "Mouse X" ) * 0.01f, 0 );
+		ground.GetComponent<MeshRenderer>().material.mainTextureOffset -= new Vector2( Input.GetAxis( "Mouse X" ) * 0.01f, 0 );
+    }
 
 	// Avoid object from spaning when selecting another one
 	public void SetSpawnObject( bool state )
