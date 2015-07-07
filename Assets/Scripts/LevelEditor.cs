8a8d5302cc4479076810a4ac54aa1dc566880121
Fernand <fernand.veyrier@epitech.eu>
2015-07-07 11:30:58 +0200

    level editor - added object dragging

--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -6,7 +6,12 @@ public class LevelEditor : MonoBehaviour {
 	public GameObject background;
 	public GameObject ground;
 	public GameObject []  propTools;
+	public GameObject objectHolder;
+	public int objectLimit;
+
 	GameObject currentTool;
+
+	int objectCount;
 
 	bool canSpawnObject;
 
@@ -15,18 +20,23 @@ public class LevelEditor : MonoBehaviour {
 	void Start () 
 	{
 		canSpawnObject = true;
+		objectCount = 0;
 	}
 	
 	void LateUpdate () 
 	{
 		if ( currentTool != null )
 		{
-			if ( Input.GetMouseButtonDown( 0 ) && canSpawnObject )
+			if ( Input.GetMouseButtonDown( 0 ) && canSpawnObject && objectCount < objectLimit )
 			{
 				go = (GameObject) Instantiate( currentTool, Camera.main.ScreenToWorldPoint( Input.mousePosition ), Quaternion.identity );
 
 				// We reset the Z axis of the obect othewise GameObject will be invisible
 				go.transform.position += new Vector3( 0, 0, Mathf.Abs( Camera.main.transform.position.z ) );
+				go.transform.SetParent( objectHolder.transform );
+				go.tag = "EditorOnly";
+				go.AddComponent<ObjectEditor>();
+				++objectCount;
 			}
 		}
 	}
@@ -38,6 +48,7 @@ public class LevelEditor : MonoBehaviour {
 			return;
 		background.GetComponent<MeshRenderer>().material.mainTextureOffset -= new Vector2( Input.GetAxis( "Mouse X" ) * 0.01f, 0 );
 		ground.GetComponent<MeshRenderer>().material.mainTextureOffset -= new Vector2( Input.GetAxis( "Mouse X" ) * 0.01f, 0 );
+		objectHolder.transform.position += new Vector3( Input.GetAxis( "Mouse X" ) * 0.2f, 0, 0 );
     }
 
 	// Avoid object from spaning when selecting another one
@@ -55,6 +66,21 @@ public class LevelEditor : MonoBehaviour {
 		else
 		{
 			Debug.LogWarning( "Missing prop tool." );
-		}	
+		}
+	}
+
+	public void SaveLevel()
+	{
+		Debug.Log( "Save level" );
+	}
+
+	public void CleanLevel()
+	{
+		GameObject [] allObjects = GameObject.FindGameObjectsWithTag( "EditorOnly" );
+
+		foreach ( GameObject go in allObjects )
+		{
+			Destroy( go );
+		}
 	}
 }
---
+++ Assets/Scripts/ObjectEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This script is attached to object when using the level editor to handle modifications
+ */
+public class ObjectEditor : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Debug.Log("mabit");
+	}
+
+	void OnMouseDrag()
+	{
+		this.transform.position = Camera.main.ScreenToWorldPoint( Input.mousePosition ) + new Vector3( 0, 0, -Camera.main.transform.position.z );
+	}
+}
