49c5529cf704195ca50c45ceb78f00b3395d89c9
gaspar_q <quentin.gasparotto@epitech.eu>
2015-07-08 09:47:26 +0200

    script online read
    Ajout du script unity pour read un fichier en ligne.
    A completer avec l'url du site une fois mis en place.

---
+++ Assets/Scripts/dynamic_level_design.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System;
+
+public class dynamic_level_design : MonoBehaviour
+{
+	// create a new instance of WebClient
+	WebClient client = new WebClient();
+
+	// Use this for initialization
+	void Start ()
+	{
+		// set the user agent to IE6
+		client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.0.3705;)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	String	GetLevelFromWeb(String levelName)
+	{
+		try
+		{
+			// actually execute the GET request
+			string ret = client.DownloadString("http://www.daveamenta.com/2008-05/c-webclient-usage/"); //"http://sitedefernand/" + levelName
+			return(ret);
+		}
+		catch (WebException we)
+		{
+			// WebException.Status holds useful information
+			Debug.Log("Web ex: " + we.Message + "\n" + we.Status.ToString());
+		}
+		catch (NotSupportedException ne)
+		{
+			// other errors
+			Debug.Log("Not supp ex :" + ne.Message);
+		}
+		return (null);
+	}
+}
